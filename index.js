db55cd6ff46db58aa6dc800b33bb5c6524aed014
bibek <abinim.bibek19@gmail.com>
2024-01-31 21:34:04 +0000

    completed

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = 0;
 
-  get(pos) {}
+    while (index < this.length && this.items[index] < item) {
+      index++;
+    }
 
-  max() {}
+    this.items.splice(index, 0, item);
 
-  min() {}
+    this.length++;
 
-  sum() {}
+    return this.items;
+  }
 
-  avg() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items[this.length - 1];
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items[0];
+  }
+  sum() {
+    return this.items.reduce((acc, item) => acc + item, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
